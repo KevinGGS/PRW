4af1d9a8d6cc0c8068344a42be2ff7bebbcc4f37
KevinGGS <gomes.kevin@aluno.ifsp.edu.br>
2023-03-14 11:45:53 -0300

    novo comit

---
+++ aula1/exercicios/1052.php
@@ -0,0 +1,40 @@
+<?php 
+$x = fgets(STDIN);
+
+if ($x == 1) {
+    echo "January" . PHP_EOL;
+} 
+else if ($x == 2) {
+    echo "February" . PHP_EOL;
+} 
+else if ($x == 3) {
+    echo "March" . PHP_EOL;
+} 
+else if ($x == 4) {
+    echo "April" . PHP_EOL;
+} 
+else if ($x == 5) {
+    echo "May" . PHP_EOL;
+} 
+else if ($x == 6) {
+    echo "June" . PHP_EOL;
+} 
+else if ($x == 7) {
+    echo "July" . PHP_EOL;
+} 
+else if ($x == 8) {
+    echo "August" . PHP_EOL;
+} 
+else if ($x == 9) {
+    echo "September" . PHP_EOL;
+} 
+else if ($x == 10) {
+    echo "October" . PHP_EOL;
+} 
+else if ($x == 11) {
+    echo "November" . PHP_EOL;
+} 
+else if ($x == 12) {
+    echo "December" . PHP_EOL;
+}
+?>
